e03c9a411e7fd290ebf5660d301be4c34ca8e095
Philippe Charrière <ph.charriere@gmail.com>
2022-07-24 06:12:47 +0200

    📝 update starting...

---
+++ wasm_modules/03-string-as-param/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	helpers "github.com/bots-garden/capsule/helpers/functions"
+)
+
+// main is required for TinyGo to compile to Wasm.
+func main() {
+	helpers.Log("starting")
+}
+
+//export add
+func add(x uint32, y uint32) uint32 {
+	// 🖐 a wasm module cannot print something
+	//fmt.Println(x,y)
+	res := x + y
+
+	helpers.Log("👋 hello world 🌍")
+
+	return res
+}
+
+//export helloWorld
+func helloWorld() uint64 {
+	strPtrPos, size := helpers.GetStringPtrPositionAndSize("👋 hello world, I'm very happy to meet you, I love what you are doing my friend")
+	return helpers.PackPtrPositionAndSize(strPtrPos, size)
+}
+
+// how to simplify ?
+// here: create an non exportable function
+// helpers/function: an exportable function that call this on
