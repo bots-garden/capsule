de5158a71e801d070d730a41f9cb17aa412315cd
Philippe Charrière <ph.charriere@gmail.com>
2022-07-28 08:55:16 +0200

    📝 update dependencies

--- wasm_modules/capsule-hello/hello.go
+++ wasm_modules/capsule-hello/hello.go
@@ -1,7 +1,8 @@
 package main
+
 // TinyGo wasm module
 import (
-	"github.com/bots-garden/capsule/hostfunctions/wasmmodule"
+	hf "github.com/bots-garden/capsule/capsulemodule/hostfunctions/wasmmodule"
 )
 
 // main is required.
@@ -11,28 +12,28 @@ func main() {
 
 func Handle(bodyReq string, headersReq map[string]string) (bodyResp string, headersResp map[string]string, errResp error) {
 
-    hf.Log("📝 body: " + bodyReq)
+	hf.Log("📝 body: " + bodyReq)
 
-    hf.Log("Content-Type: " + headersReq["Content-Type"])
-    hf.Log("Content-Length: " + headersReq["Content-Length"])
-    hf.Log("User-Agent: " + headersReq["User-Agent"])
+	hf.Log("Content-Type: " + headersReq["Content-Type"])
+	hf.Log("Content-Length: " + headersReq["Content-Length"])
+	hf.Log("User-Agent: " + headersReq["User-Agent"])
 
-    envMessage, err := hf.GetEnv("MESSAGE")
-    if err != nil {
-        hf.Log("😡 " + err.Error())
-    } else {
-        hf.Log("Environment variable: " + envMessage)
-    }
+	envMessage, err := hf.GetEnv("MESSAGE")
+	if err != nil {
+		hf.Log("😡 " + err.Error())
+	} else {
+		hf.Log("Environment variable: " + envMessage)
+	}
 
-    headersResp = map[string]string{
-        "Content-Type": "application/json; charset=utf-8",
-        "Message": "👋 hello world 🌍",
-    }
+	headersResp = map[string]string{
+		"Content-Type": "application/json; charset=utf-8",
+		"Message":      "👋 hello world 🌍",
+	}
 
-    bodyResp = `{"message": "Hello how are you?"}`
+	bodyResp = `{"message": "Hello how are you?"}`
 
-	return bodyResp, headersResp , nil
-    //return bodyResp, headersResp , errors.New("😡 oups I did it again")
+	return bodyResp, headersResp, nil
+	//return bodyResp, headersResp , errors.New("😡 oups I did it again")
 }
 
 // TODO: helpers: SetHeader() ...
