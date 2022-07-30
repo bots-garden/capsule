0e9f0f84079c7c5bb6520bb209fa28ff2cf01113
Philippe Charrière <ph.charriere@gmail.com>
2022-07-30 10:13:09 +0200

    ✨ feature load the wasm module from an url (#62)

--- wasm_modules/capsule-hello/run-http-test.sh
+++ wasm_modules/capsule-hello/run-http-test.sh
@@ -6,6 +6,3 @@ go run main.go \
    -wasm=../wasm_modules/capsule-hello/hello.wasm \
    -mode=http \
    -httpPort=7070
-
-# -wasm=./wasm_modules/capsulelauncher-function-template/hello.wasm \
-# -wasm=./wasm_modules/capsulelauncher-http/hello.wasm \
