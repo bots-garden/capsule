695e54d4cbafc9956aab79cffcf9de7138b84b67
Adrian Cole <adrian@tetrate.io>
2022-08-31 16:57:55 +0800

    deps: updates wazero to 1.0.0-pre.1
    renames the classifier used before 1.0 per https://github.com/tetratelabs/wazero/releases/tag/v1.0.0-pre.1
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- capsule-launcher/services/wasmrt/wasmrt.go
+++ capsule-launcher/services/wasmrt/wasmrt.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
-	"github.com/tetratelabs/wazero/wasi_snapshot_preview1"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
 )
 
 // GetPackedPtrPositionAndSize :
