8065930e96281c88fb619c12ca73ee3a6328c5f6
Philippe Charrière <ph.charriere@gmail.com>
2022-09-25 08:07:37 +0200

    📝 save

--- commons/version.go
+++ commons/version.go
@@ -1,9 +1,6 @@
 package commons
 
 func CapsuleVersion() string {
-	return "v0.2.5 🦖 [T-Rex]"
-	/*
-	   return "v0.2.6 🐝 [Bee][dev]"
-	   //next "v0.2.6 🐝 [Bee]"
-	*/
+	return "v0.2.6 🐝 [Bee][dev]"
+	//next "v0.2.6 🐝 [Bee]"
 }
