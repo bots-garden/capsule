66c369cf1afb0dd8cdcc8c2da95892243624ede5
Philippe Charrière <ph.charriere@gmail.com>
2022-11-07 19:06:49 +0100

    👽️ Update code due to external API changes: hostGetExitError, hostGetExitCode translated

--- wasm_modules/capsule-hello-post-next/small-stress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-hey -n 50 -c 2 -m POST \
--H "Content-Type: application/json" \
--d '{"message": "Golang 💚💜 wasm", "author": "Philippe"}' \
-"http://localhost:7070"
-
--- wasm_modules/capsule-hello-post-next/stress.sh
+++ wasm_modules/capsule-hello-post-next/stress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-hey -n 5000 -c 50 -m POST \
+#hey -n 5000 -c 50 -m POST \
+hey -n 300 -c 100 -m POST \
 -H "Content-Type: application/json" \
 -d '{"message": "Golang 💚💜 wasm", "author": "Philippe"}' \
 "http://localhost:7070"
