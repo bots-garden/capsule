6f91503a33bc8fba3661ce5df1991346c8b53f9f
k33g <ph.charriere@gmail.com>
2023-05-21 21:09:45 +0200

    💾 Saved: save current status

--- capsule-http/handlers/faas.go
+++ capsule-http/handlers/faas.go
@@ -3,8 +3,14 @@ package handlers
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 	"sync"
+	"time"
 
+	"github.com/bots-garden/capsule/capsule-http/tools"
+	"github.com/go-resty/resty/v2"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -16,11 +22,69 @@ var runningCapsules sync.Map
 // It takes in a pointer to a fiber.Ctx object.
 // It returns an error object.
 func StartCapsuleHTTP(c *fiber.Ctx) error {
+	//TODO check if the process exists
 
 	functionName := c.Params("function_name")
 	functionRevision := c.Params("function_revision") // not sure if I will use it
+	httpPort := tools.GetHTTPPort()
 	// TODO: store somewhere the processes that are running (or not)
-	fmt.Println(functionName, functionRevision)
-	
-	return nil
+
+	fmt.Println(functionName, functionRevision, httpPort)
+
+	args := []string{
+		"",
+		"-wasm=./functions/hello-world/hello-world.wasm",
+		"-httpPort="+ httpPort,
+	}
+	// try without the httport too
+
+	cmd := &exec.Cmd{
+		Path:   "./capsule-http",
+		Args:   args,
+		Stdout: os.Stdout,
+		Stderr: os.Stdout,
+	}
+	newEnv := append(os.Environ(), []string{"MESSAGE=Hello"}...)
+	cmd.Env = newEnv
+
+	err := cmd.Start()
+
+	if err != nil {
+		log.Println("🚗", err.Error())
+	} else {
+		log.Println("🚙", cmd.Args)
+	}
+
+	//TODO make an HTTP request to the function
+	bodyRequest := c.Body()
+	//headersRequest := c.GetReqHeaders()
+
+	httpClient := resty.New()
+
+	/*
+	for key, value := range req.Headers {
+		httpClient.SetHeader(key, value)
+	}
+	*/
+	time.Sleep(1 * time.Second) // Wait for 1 seconds
+	// TODO use health check before launch
+
+
+	//TODO test request method
+	resp, err := httpClient.R().EnableTrace().SetBody(string(bodyRequest)).Post("http://localhost:" + httpPort)
+
+	if err != nil {
+		return c.Send([]byte(err.Error()))
+	} 
+	return c.Send([]byte(resp.String()))
+
 }
+
+/*
+   "path": "./services/capsule/capsule-http",
+   "args": [
+     "",
+     "-wasm=./services/capsule/hello-world.wasm",
+     "-httpPort=59746"
+   ],
+*/
--- capsule-http/query.sh
+++ capsule-http/query.sh
@@ -10,3 +10,17 @@ curl -X POST http://localhost:8080 \
 #          -H 'Content-Type: application/json; charset=utf-8' \
 #          -d "${JSON_DATA}"
 
+#curl -X POST http://localhost:8080/functions/yo/blue
+
+curl -X POST http://localhost:38771/
+
+42723
+
+curl -X POST http://localhost:42723 \
+          -H 'Content-Type: application/json; charset=utf-8' \
+          -d '{"name":"Bob Morane","age":42}'
+
+
+curl -X POST http://0.0.0.0:8080/functions/yo/blue \
+          -H 'Content-Type: application/json; charset=utf-8' \
+          -d '{"name":"Bob Morane","age":42}'
