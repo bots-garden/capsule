e8edb0fffb657606c6a4f91315a8d104b9d7c0e0
k33g <ph.charriere@gmail.com>
2023-06-06 17:36:27 +0200

    🛟 Updated: index page is a process on faas mode

--- capsule-http/handlers/faas.call.go
+++ capsule-http/handlers/faas.call.go
@@ -28,6 +28,7 @@ import (
 // ```
 var capsuleDomain = tools.GetEnv("CAPSULE_DOMAIN", "http://0.0.0.0")
 
+
 // CallExternalFunction calls the WASM module of another Capsule HTTP process
 // ! this a work in progress
 // c *fiber.Ctx: a pointer to a fiber context object that contains information about the http request.
@@ -244,7 +245,91 @@ func CallExternalFunction(c *fiber.Ctx) error {
 
 }
 
-
+// CallExternalIndexPageFunction is called by the main process of the faas mode
+// to call the index.page.wasm 
+// the function name is "index.page"
+// only GET method is supported
 func CallExternalIndexPageFunction(c *fiber.Ctx) error {
-	return c.Send([]byte("Hello, World!"))
+
+	// register the last call
+	SetLastCall(time.Now())
+
+	functionName := "index.page" // => "index.page.wasm" 
+	functionRevision := "default"
+	functionIndex := "0"
+
+	// the unique key to identify a Capsule Process
+	key := functionName + "/" + functionRevision + "/" + functionIndex
+
+	process, err := data.GetCapsuleProcessRecord(key)
+
+	if err != nil {
+		// The process does not exist (no "index.page.wasm" file)
+		// instead, display a default message
+		return c.SendString("Capsule " + tools.GetVersion() + "[faas]")
+
+	}
+
+	headersRequest := c.GetReqHeaders()
+	httpClient := resty.New()
+
+	for key, value := range headersRequest {
+		httpClient.SetHeader(key, value)
+	}
+
+	capsuleURI := capsuleDomain + ":" + process.HTTPPort
+	
+	restyHeadersToFiberHeaders := func(resp *resty.Response) {
+		for key, value := range resp.Header() {
+			c.Response().Header.Set(key, value[0])
+		}
+	}
+
+	retryToConnect := func(freq func() (*resty.Response, error)) (*resty.Response, error) {
+		// Retry the HTTP request
+		var resp *resty.Response
+		// Wait until the process is running/ready
+		for i := 1; i < 5; i++ {
+			time.Sleep(time.Millisecond * 500) // ? parameter or fixed?
+			resp, err = freq()
+			if err == nil {
+				log.Println("🟢 Successfully restarted the Capsule process")
+				break
+			}
+		}
+		return resp, err
+	}
+
+	getRequest := func() (*resty.Response, error) {
+		resp, err := httpClient.R().EnableTrace().Get(capsuleURI)
+		restyHeadersToFiberHeaders(resp)
+		c.Status(resp.StatusCode())
+		return resp, err
+	}
+	resp, err := getRequest()
+
+	if err != nil { // That means that the Capsule process is not running
+		if strings.Contains(err.Error(), "connection refused") {
+			// Try to restart the process (in case it already exists in the process list)
+			errStart := ReStartCapsuleHTTPProcess(c)
+			if errStart != nil {
+				return c.Send([]byte(err.Error()))
+			}
+			// Retry the HTTP request
+			// Wait until the process is running/ready
+			resp, err := retryToConnect(getRequest)
+
+			if err != nil {
+				// Probably the procesess never existed before
+				return c.Send([]byte(err.Error()))
+			}
+			// Else return the response
+			return c.Send([]byte(resp.String()))
+		}
+
+		return c.Send([]byte(err.Error()))
+	}
+
+	return c.Send([]byte(resp.String()))
+
 }
